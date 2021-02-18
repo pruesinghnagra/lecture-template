9b3f176fcee6f5145e7a892a5835d3d3f8cbabbb
pruesinghnagra <pruesinghnagra@users.noreply.github.com>
2021-02-18 14:58:47 +1300

    Initial commit

---
+++ client/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <main className="col-6">
+      <h1 className="title">Boil</h1>
+      <p className="grid-6">
+        This is CSS grid
+      </p>
+    </main>
+  )
+}
+
+export default App
---
+++ client/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path')
+
+module.exports = {
+  entry: ['./client/index.js', './client/styles/main.scss'],
+  output: {
+    path: path.join(__dirname, '..', 'server', 'public'),
+    filename: 'bundle.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map'
+}
